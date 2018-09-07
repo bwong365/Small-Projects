c9b5e7fb4ff615c657c1f2b43f63da0160b2a793
Oop365 <42486090+bwong365@users.noreply.github.com>
2018-09-07 02:27:21 -0600

    First change
    Moved variable declaration and initialization for basicSeries into the basicSeries method.

--- Converge on Pi/src/calc/CalculatePi.java
+++ Converge on Pi/src/calc/CalculatePi.java
@@ -1,24 +1,23 @@
 package calc;
 
 public class CalculatePi {
-    // I guess we'll start with the simplest convergence
-    // 4/1 - 4/3 + 4/5 - 4/7 + 4/9...
-    int numerator;
-    int denominator;
-
-    // on for plus, off for minus
-    boolean plusOrMinus;
-
+    
     public CalculatePi() {
-        numerator = 4;
-        denominator = 1;
-        plusOrMinus = true;
+
     }
 
+    /**
+     * // I guess we'll start with the simplest convergence
+     *     // 4/1 - 4/3 + 4/5 - 4/7 + 4/9...
+     *  two billion iterations gets us to 10 millionths accuracy
+     */
     public Double basicSeries() {
+        int numerator = 4;
+        int denominator = 1;
+        // on for plus, off for minus
+        boolean plusOrMinus= true;
         double pi = 0.0;
 
-        // two billion iterations gets us to 10 millionths accuracy
         // we swap the operator and increase the denominator each time
         for (int i = 0; i < 2000000000; i++) {
             double fr = numerator/(double) denominator;
