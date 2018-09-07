172b6e3f77ceab7bd3682814c4e043d40370b610
Oop365 <42486090+bwong365@users.noreply.github.com>
2018-09-07 02:50:08 -0600

    Exploring BigDecimal
    This is an initial exploration of the BigDecimal class. I'll have to read more of the documentation in order to understand rounding behavior and proper declaring of the variables.

--- Converge on Pi/src/Main.java
+++ Converge on Pi/src/Main.java
@@ -3,7 +3,7 @@ import calc.CalculatePi;
 public class Main {
     public static void main(String[] args) {
         CalculatePi calc = new CalculatePi();
-        System.out.println(calc.basicSeries());
+        System.out.println(calc.basicSeriesBd());
 
     }
 }
--- Converge on Pi/src/calc/CalculatePi.java
+++ Converge on Pi/src/calc/CalculatePi.java
@@ -1,7 +1,10 @@
 package calc;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class CalculatePi {
-    
+
     public CalculatePi() {
 
     }
@@ -15,7 +18,7 @@ public class CalculatePi {
         int numerator = 4;
         int denominator = 1;
         // on for plus, off for minus
-        boolean plusOrMinus= true;
+        boolean plusOrMinus = true;
         double pi = 0.0;
 
         // we swap the operator and increase the denominator each time
@@ -31,6 +34,36 @@ public class CalculatePi {
             }
 
             denominator += 2;
+        }
+
+        return pi;
+    }
+
+    /**
+     * Staying with the simple method, as I explore how to use BigDecimal.
+     * At least for this method, BigDecimal adds a decimal point of additional
+     * while increasing the timeframe by an absurd amount
+     */
+    public BigDecimal basicSeriesBd() {
+        BigDecimal numerator = new BigDecimal("4.00000000000000000000000");
+        BigDecimal denominator = new BigDecimal(1.0);
+        // on for plus, off for minus
+        boolean plusOrMinus = true;
+        BigDecimal pi = new BigDecimal(0.0);
+
+        // we swap the operator and increase the denominator each time
+        for (int i = 0; i < 2000000000; i++) {
+            BigDecimal fr = numerator.divide(denominator, RoundingMode.HALF_UP);
+
+            if (plusOrMinus) {
+                pi = pi.add(fr);
+                plusOrMinus = false;
+            } else {
+                pi = pi.subtract(fr);
+                plusOrMinus = true;
+            }
+
+            denominator = denominator.add(BigDecimal.valueOf(2));
         }
 
         return pi;
